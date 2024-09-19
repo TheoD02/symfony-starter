b001c97c7b81e482fe3c38f2dfa76e86aa94dba0
Theo D <theo.d02290@gmail.com>
2024-09-19 23:50:50 +0200

    fix(ui): role fetcher

--- app/assets/security/roles-fetcher.ts
+++ app/assets/security/roles-fetcher.ts
@@ -1,24 +1,26 @@
 import * as fs from 'fs';
 
 const fetchRoles = async () => {
-  const response = await fetch('http://localhost/api/role-fetcher');
+  const response = await fetch('http://localhost/api/users/roles');
   const data = await response.json();
 
   return data;
 };
 
-const createEnumFileFromRoles = (roles: string[]) => {
-  const text = `enum Roles {
-${roles.reduce((acc, role) => {
-  acc += `  ${role} = '${role}',\n`;
-  return acc;
-}, '')}
-}
+const createEnumFileFromRoles = (groups: [string, string][]) => {
+  let text = 'enum Roles {\n';
+  groups.forEach(([group, roles]) => {
+    roles.forEach((role) => {
+      text += `  ${role} = '${role}',\n`;
+    });
+  });
 
-export default Roles;`;
+  text += '}\n\n';
+
+  text += 'export default Roles;';
 
   console.log('Fetching roles and creating enum file');
   return fs.writeFileSync('assets/security/roles.ts', text);
 }
 
-fetchRoles().then(createEnumFileFromRoles);
+fetchRoles().then((data) => createEnumFileFromRoles(Object.entries(data['hydra:member'] || [])));
